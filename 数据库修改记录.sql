2c33fdbcdc8f13a8ce5cc1e109503c9fee66d1c6
mikasa007 <17863109506@163.com>
2020-01-15 12:21:57 +0800

    CMS修改

--- 数据库修改记录.sql
+++ 数据库修改记录.sql
@@ -1,5 +1,5 @@
 /*---------------------------------创建功能表---------------------------------------*/
-CREATE TABLE  cms_ath_function (
+CREATE TABLE  cms_pub_function (
                                    function_id varchar(64) not null comment '功能ID',
                                    function_name nvarchar(64) not null  comment  '功能名字',
                                    function_url varchar(64) comment '功能url',
@@ -7,5 +7,5 @@ CREATE TABLE  cms_ath_function (
                                    parentFunction varchar(64) comment  '父类功能',
                                    primary key (function_id),
                                    unique (function_id,function_name,function_url),
-                                  constraint fk_function_id foreign key  (parentFunction)  references cms_ath_function(function_id)
+                                  constraint fk_function_id foreign key  (parentFunction)  references cms_pub_function(function_id)
 )  comment ='功能表' default charset = utf8;
